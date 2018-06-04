66a6c0ccc339f17475a9ff8382a4adb954a6ea2c
Rene Lam <renelam1421@gmail.com>
2018-06-04 16:08:46 -0700

    HW3

--- build.py
+++ build.py
@@ -1,20 +1,57 @@
-top = open('templates/top.html').read()
-bottom = open('templates/bottom.html').read()
+pages = [
+    {
+        'filename': 'content/index.html',
+        'title': 'Index',
+    },
+    {
+        'filename': 'content/about.html',
+        'title': 'About',
+    },
+    {
+        'filename': 'content/contact.html',
+        'title': 'Contact',
+    },
+    {
+        'filename': 'content/posts.html',
+        'title': 'Posts',
+    },
+    {
+        'filename': 'content/projects.html',
+        'title': 'Projects',
+    },
+]
 
-index = open('content/index.html').read()
-about = open('content/about.html').read()
-contact = open('content/contact.html').read()
-posts = open('content/posts.html').read()
-projects = open('content/projects.html').read()
+def main():
+    template = open('templates/base.html').read()
 
-combine_index = top + index + bottom
-combine_about = top + about + bottom
-combine_contact = top + contact + bottom
-combine_posts = top + posts + bottom
-combine_projects = top + projects + bottom
+    for item in pages:
+        content = open(item['filename']).read()
+        finished_content = template.replace('{{content}}', content)
+        page_name = str('docs/' + item['title'] + '.html')
+        open(page_name, 'w+').write(finished_content)
 
-open('docs/index.html', 'w').write(combine_index)
-open('docs/about.html', 'w').write(combine_about)
-open('docs/contact.html', 'w').write(combine_contact)
-open('docs/posts.html', 'w').write(combine_posts)
-open('docs/projects.html', 'w').write(combine_projects)
+if __name__ == '__main__':
+  main()
+
+
+
+# top = open('templates/top.html').read()
+# bottom = open('templates/bottom.html').read()
+#
+# index = open('content/index.html').read()
+# about = open('content/about.html').read()
+# contact = open('content/contact.html').read()
+# posts = open('content/posts.html').read()
+# projects = open('content/projects.html').read()
+#
+# combine_index = top + index + bottom
+# combine_about = top + about + bottom
+# combine_contact = top + contact + bottom
+# combine_posts = top + posts + bottom
+# combine_projects = top + projects + bottom
+#
+# open('docs/index.html', 'w').write(combine_index)
+# open('docs/about.html', 'w').write(combine_about)
+# open('docs/contact.html', 'w').write(combine_contact)
+# open('docs/posts.html', 'w').write(combine_posts)
+# open('docs/projects.html', 'w').write(combine_projects)
